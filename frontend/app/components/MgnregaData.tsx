8d72549cc77ee17bd4228a5480e0da90d2fb4521
FutureTL <futuretechnologistics21@gmail.com>
2025-10-28 22:50:48 +0530

    frontend code added

---
+++ frontend/app/components/MgnregaData.tsx
@@ -0,0 +1,91 @@
+"use client"
+import React, {useState} from 'react';
+import axios from 'axios';
+import { State, City , IState, ICity} from 'country-state-city';
+
+const MgnregaData: React.FC = () => {
+
+    const [state, setState] = useState<string>("")
+    const [district, setDistrict] = useState<string>("")
+    // const [year, setYear] = useState<string>("")
+    const [data, setData] = useState<any>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+
+    // this variable takes states in an array in the format if IStates that comes in the lib
+    const indianStates: IState[] = State.getStatesOfCountry("IN");
+    const districtsInState: ICity[]= state ? City.getCitiesOfState("IN",state): [];
+
+    const fetchData = async ()=>{
+        // fetch data from backend
+        setLoading(true);
+        
+        await axios.get("http://localhost:8000/fetch_api_data", {
+            params:{
+                state_name: indianStates.find((s)=> s.isoCode === state)?.name,
+                district_name: district
+            }
+        }).then((res: { data: { data: any; }; })=>{
+            console.log("backend response ", res.data.data);
+            setData(res.data.data);
+
+        }).catch((err: any)=>{
+            console.log("error in fetching data from backend", err);
+
+        }).finally(()=>{
+            setLoading(false);
+        })
+
+    }
+
+    return(
+        
+       <div>
+            <h1>Mgnrega Website</h1>
+                <select
+                    value={state}
+                    onChange={(e)=>{
+                        setState(e.target.value);
+                        setDistrict("");
+                    }}
+                >
+                    <option value="">Select state</option>
+                    {indianStates.map((s)=>(
+                        <option key={s.isoCode} value={s.isoCode}>
+                            {s.name}
+                        </option>
+                    ))}
+                </select>
+
+                <select
+                    value={district}
+                    onChange={(e)=>(
+                        setDistrict(e.target.value))}
+                        disabled={!state}
+                >
+                    <option value="">Select District</option>
+                    {districtsInState.map((d)=>(
+                        <option key={d.name} value={d.name}>
+                            {d.name}
+                        </option>
+                    ))}
+                </select>
+
+                <button
+                    onClick={fetchData} disabled={!state || loading}
+                >
+                    {loading ? "Loading..." : "Fetch Data"}
+                </button>
+
+                {data && ( 
+                    <pre style={{ textAlign: "left" }}> 
+                        {JSON.stringify(data, null, 2)} 
+                    </pre>
+                 )}
+
+       </div>
+    
+        
+    )
+}
+
+export default MgnregaData;
